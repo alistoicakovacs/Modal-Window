1a70fe9bf2be10b74ea809ea696353f21fc2c9b0
Ali Stoica Kovacs <78688064+alistoicakovacs@users.noreply.github.com>
2022-04-07 22:07:49 +0200

    Add files via upload

--- Modal/script.js
+++ Modal/script.js
@@ -3,9 +3,9 @@ const closeButton = document.querySelector(".close-button");
 const modal = document.querySelector(".modal");
 
 closeButton.addEventListener("click", function () {
-  modal.classList.add("hidden");
+  modal.classList.remove("active");
 });
 
 openButton.addEventListener("click", function () {
-  modal.classList.remove("hidden");
+  modal.classList.add("active");
 });
